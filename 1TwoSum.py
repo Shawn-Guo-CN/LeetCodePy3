414392955650c9bcd4e87395985c6d6b73dd7b50
Shawn Guo <shawnguo.cn@gmail.com>
2018-12-22 17:55:14 +0000

    first 3 questions

---
+++ 1TwoSum.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/22 17:52
+# @Author  : Shawn
+# @File    : 1TwoSum.py
+
+class Solution:
+    def twoSum_bak(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        map = {}
+
+        for index, num in enumerate(nums):
+            if target - num in map:
+                return [map[target - num], index]
+            map[num] = index
---
+++ 3LongestSubstrWithoutRepeatingChars.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/22 17:54
+# @Author  : Shawn
+# @File    : 3LongestSubstrWithoutRepeatingChars.py
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        start = -1
+        max = 0
+        d = {}
+
+        for i in range(len(s)):
+            if s[i] in d and d[s[i]] > start:
+                start = d[s[i]]
+                d[s[i]] = i
+            else:
+                d[s[i]] = i
+                if i - start > max:
+                    max = i - start
+        return max
